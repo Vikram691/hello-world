06e09222975fea3910b22c3946d66c5b1f7b0961
Vikram691 <dasaramvikram@gmail.com>
2022-03-11 11:20:49 +0530

    This is my first shell code

--- Input.sh
+++ Input.sh
@@ -1,7 +1,12 @@
-
 #! bin/bash
 
 read -p "Enter your name:" name
 echo "your name=$name"
 
+#spoecial Variables
+# $0-$n , $* / $@, $#
+echo Script name= $0
+echo First Argument= $1
+echo All Arguments=$*
+echo Number of Arguments=$#
 
