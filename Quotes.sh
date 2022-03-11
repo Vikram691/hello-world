1a388c4245980d778c095cd00710df8a32e807de
Vikram691 <dasaramvikram@gmail.com>
2022-03-11 17:31:05 +0530

    This is my first shell code

--- Quotes.sh
+++ Quotes.sh
@@ -1,4 +1,8 @@
 #! bin/bash
 
-Lenovo Cost is $1000
-Lenovo Cost is $1000
+echo Lenovo 'Cost is $1000'
+echo Lenovo "Cost is $1000"
+
+TRAINING=Devops
+echo 'Training=${TRAINING}'
+echo "Training=${TRAINING}"
