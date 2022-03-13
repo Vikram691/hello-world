f4b0596ffa47c2d9ee33303826ed572817e30d7d
Vikram691 <dasaramvikram@gmail.com>
2022-03-13 17:07:41 +0530

    This is my first shell code

---
+++ Functions.sh
@@ -0,0 +1,19 @@
+#function_name{
+#command
+#command
+}
+
+#function_name
+
+#Declare a function
+
+Print_Message(){
+  echo Hello,
+  echo Good Morning,
+  echo welcome to Devops training
+
+}
+
+#Main Program
+
+#Print_Message
