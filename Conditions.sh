abe74510f04e6165c3ec6f0282820be1faddd45a
Vikram691 <dasaramvikram@gmail.com>
2022-03-19 18:39:29 +0530

    This is Make File

--- Conditions.sh
+++ Conditions.sh
@@ -4,3 +4,16 @@ if [ 1 -eq  1 ]
 then
   echo hello
   fi
+
+a="abc"
+if [ "$a" == "abc" ]; then
+  echo Both are equal
+  fi
+
+if [ "$a" != "abc"]; then
+  echo Both are not equal
+  fi
+
+if [-z "$b" ]; then
+  echo b variable is empty
+  fi
