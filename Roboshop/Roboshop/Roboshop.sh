78a600275301305d0b159da5c7a246cdcef72405
Vikram691 <dasaramvikram@gmail.com>
2022-03-20 14:20:34 +0530

    This is Make File

--- Roboshop/Roboshop/Roboshop.sh
+++ Roboshop/Roboshop/Roboshop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ ! -e Components/$1.sh ]; then
-  echo "component doesn't exist"
-  exit
-  fi
+#if [ ! -e Components/$1.sh ]; then
+ # echo "component doesn't exist"
+  #exit
+  #fi
 component/$1.sh
