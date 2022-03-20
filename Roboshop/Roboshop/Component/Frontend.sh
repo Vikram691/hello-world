1741633c6f4236a85a75bb72a9c0fab5c3e46a16
Vikram691 <dasaramvikram@gmail.com>
2022-03-20 13:51:34 +0530

    This is Make File

--- Roboshop/Roboshop/Component/Frontend.sh
+++ Roboshop/Roboshop/Component/Frontend.sh
@@ -7,19 +7,19 @@ if [ "$USER_ID" -ne 0 ]; then
   exit 1
   fi
 
-
-
 echo Frontend component
-echo -e installing niginx
+
+echo -e "\e[32m installing niginx\e[0m"
 yum install nginx -y
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
-  echo -e successfully installed
+  echo -e "\e[36m successfully installed\e[0m"
   else
     echo -e Failed
     exit 2
     fi
 
+echo -e "\e[36m cleanup old nginx and extractive new downloaded archive\e[om"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
@@ -30,6 +30,19 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
  mv static/* .
  rm -rf frontend-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m successful\e[0m"
+  else
+    echo -e "\e[33m Failed\e[0m"
+    exit 2
+    fi
+ echo -e "\e[32m stating nginx\e[0m"
 systemctl restart nginx
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m successful\e[0m"
+  else
+    echo -e "\e[33m Failed\e[0m"
+    exit 2
+    fi
+
 systemctl enable nginx
